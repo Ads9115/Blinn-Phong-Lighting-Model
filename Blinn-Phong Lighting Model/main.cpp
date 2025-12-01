77d6e431c2cda8abbe9657378fbec4eb351f0682
adars <adarshsen9115@gmail.com>
2025-12-02 01:36:46 +0530

    added aNormal in vertexShader.vert

--- Blinn-Phong Lighting Model/main.cpp
+++ Blinn-Phong Lighting Model/main.cpp
@@ -92,6 +92,9 @@ int main() {
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
 
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+	glEnableVertexAttribArray(1);
+
     glBindBuffer(GL_ARRAY_BUFFER, 0);
     glBindVertexArray(0);
 
