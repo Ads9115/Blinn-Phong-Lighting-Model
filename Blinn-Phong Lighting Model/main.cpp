b3bdc4ed12395fa3e5bfaed625d81b79255beacb
adars <adarshsen9115@gmail.com>
2025-12-01 22:53:17 +0530

    minor adjustment

--- Blinn-Phong Lighting Model/main.cpp
+++ Blinn-Phong Lighting Model/main.cpp
@@ -1,40 +1,13 @@
-#include <glad/glad.h>  // <-- Must be included BEFORE GLFW
-#include <GLFW/glfw3.h>
-#include <iostream>
+#include<iostream>
+#include"glad/glad.h"
+#include"GLFW/glfw3.h"
 
-// Include your custom math header
-#include "glMath.h"
+#include"Shader.h"
 
-// --- Window Settings ---
 const unsigned int SCR_WIDTH = 800;
 const unsigned int SCR_HEIGHT = 600;
 
-// --- Shaders (OpenGL 3.3 Core) ---
-const char* vertexShaderSource = R"glsl(
-    #version 330 core
-    layout (location = 0) in vec3 aPos;
 
-    uniform mat4 model;
-    uniform mat4 view;
-    uniform mat4 projection;
-
-    void main()
-    {
-        gl_Position = projection * view * model * vec4(aPos, 1.0);
-    }
-)glsl";
-
-const char* fragmentShaderSource = R"glsl(
-    #version 330 core
-    out vec4 FragColor;
-
-    void main()
-    {
-        FragColor = vec4(1.0, 0.5, 0.2, 1.0); // Orange
-    }
-)glsl";
-
-// --- Cube Data ---
 float vertices[] = {
     -0.5f, -0.5f, -0.5f,
      0.5f, -0.5f, -0.5f,
@@ -45,6 +18,8 @@ float vertices[] = {
      0.5f,  0.5f,  0.5f,
     -0.5f,  0.5f,  0.5f
 };
+
+
 unsigned int indices[] = {
     0, 1, 2,  2, 3, 0, // Back
     4, 5, 6,  6, 7, 4, // Front
@@ -54,7 +29,6 @@ unsigned int indices[] = {
     0, 1, 5,  5, 4, 0  // Bottom
 };
 
-// Shader compilation helper
 GLuint compileShader(const char* source, GLenum type) {
     GLuint shader = glCreateShader(type);
     glShaderSource(shader, 1, &source, NULL);
@@ -69,50 +43,38 @@ GLuint compileShader(const char* source, GLenum type) {
     return shader;
 }
 
+
 int main() {
-    // Initialize GLFW
-    if (!glfwInit()) {
-        std::cerr << "Failed to initialize GLFW" << std::endl;
-        return -1;
-    }
-    // OpenGL 3.3 Core
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-    GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "Custom Math Cube (GLAD)", NULL, NULL);
-    if (!window) {
-        std::cerr << "Failed to create GLFW window" << std::endl;
-        glfwTerminate();
-        return -1;
-    }
-    glfwMakeContextCurrent(window);
+	glfwInit();
 
-    // Initialize GLAD 
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE,GLFW_OPENGL_CORE_PROFILE);
 
-    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
-        std::cerr << "Failed to initialize GLAD" << std::endl;
-        return -1;
-    }
+	GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "Blinn Phong Lighting Model", NULL, NULL);
 
-    // Set viewport and enable depth testing
-    glViewport(0, 0, SCR_WIDTH, SCR_HEIGHT);
-    glEnable(GL_DEPTH_TEST);
+	if (!window) {
 
-    // Build Shader Program 
-    GLuint vs = compileShader(vertexShaderSource, GL_VERTEX_SHADER);
-    GLuint fs = compileShader(fragmentShaderSource, GL_FRAGMENT_SHADER);
+		std::cerr << "Cannot Make the window" << std::endl;
 
-    GLuint shaderProgram = glCreateProgram();
-    glAttachShader(shaderProgram, vs);
-    glAttachShader(shaderProgram, fs);
-    glLinkProgram(shaderProgram);
+	}
 
-    glDeleteShader(vs);
-    glDeleteShader(fs);
+	glfwMakeContextCurrent(window);
 
-    // Set up Buffers (VAO, VBO, EBO)
-    GLuint VAO, VBO, EBO;
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+
+		std::cerr << "Failed to initialize GLAD" << std::endl;
+	}
+
+	glViewport(0, 0, SCR_WIDTH, SCR_HEIGHT);
+	glEnable(GL_DEPTH_TEST);
+
+	Shader shaderProgram("vertexShader.vert", "fragmentShader.frag");
+
+    
+
+    unsigned int VAO, VBO, EBO;
     glGenVertexArrays(1, &VAO);
     glGenBuffers(1, &VBO);
     glGenBuffers(1, &EBO);
@@ -131,72 +93,36 @@ int main() {
     glBindBuffer(GL_ARRAY_BUFFER, 0);
     glBindVertexArray(0);
 
-
-    glUseProgram(shaderProgram);
-    GLint modelLoc = glGetUniformLocation(shaderProgram, "model");
-    GLint viewLoc = glGetUniformLocation(shaderProgram, "view");
-    GLint projLoc = glGetUniformLocation(shaderProgram, "projection");
+	shaderProgram.use();
 
 
-    while (!glfwWindowShouldClose(window)) {
-
-        if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
-            glfwSetWindowShouldClose(window, true);
-
-        // Render 
-        glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
-        glUseProgram(shaderProgram);
-
-        //Mathlib.h
 
 
-        mat4 projection = mat4::perspective(
-            radians(45.0f),                       // fov
-            (float)SCR_WIDTH / (float)SCR_HEIGHT, // aspect
-            0.1f,                                 // zNear
-            100.0f                                // zFar
-        );
 
-        // 2. View (lookAt)
-        mat4 view = mat4::lookAt(
-            vec3(0.0f, 1.0f, 4.0f),  // eye
-            vec3(0.0f, 0.0f, 0.0f),  // center
-            vec3(0.0f, 1.0f, 0.0f)   // up
-        );
+	while (!glfwWindowShouldClose(window)) {
 
-        // 3. Model (rotate)
-        mat4 model = mat4::identity();
-        model = model * mat4::rotate(
-            radians((float)glfwGetTime() * 30.0f),
-            vec3(0.0f, 1.0f, 0.0f)
-        );
-        model = model * mat4::rotate(
-            radians((float)glfwGetTime() * 20.0f),
-            vec3(1.0f, 0.0f, 0.0f)
-        );
+		if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+			glfwSetWindowShouldClose(window, true);
 
-        //Pass Matrices to Shader
-        glUniformMatrix4fv(projLoc, 1, GL_FALSE, projection.ptr());
-        glUniformMatrix4fv(viewLoc, 1, GL_FALSE, view.ptr());
-        glUniformMatrix4fv(modelLoc, 1, GL_FALSE, model.ptr());
+		glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-        //Draw the Cube
+
         glBindVertexArray(VAO);
         glDrawElements(GL_TRIANGLES, 36, GL_UNSIGNED_INT, 0);
 
-        //Swap Buffers & Poll Events
-        glfwSwapBuffers(window);
-        glfwPollEvents();
-    }
+		glfwSwapBuffers(window);
+		glfwPollEvents();
 
+	}
 
     glDeleteVertexArrays(1, &VAO);
     glDeleteBuffers(1, &VBO);
     glDeleteBuffers(1, &EBO);
-    glDeleteProgram(shaderProgram);
+    
 
     glfwTerminate();
-    return 0;
+
+	return 0;
+
 }
